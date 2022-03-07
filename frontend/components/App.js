f98c97a724bbccb92755c8c589b16c83f3bdf2d9
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-07 15:11:58 -0800

    state is set

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,65 @@
 import React from 'react'
 
+
+
+
+const todos = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    name: 'Eat Lunch',
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    name: 'Eat Dinner',
+    id: 1528817084358,
+    completed: true
+  },
+]
+  
+  
+
+
+const initialState = { todos }
+
+
+
+
+
+
+
+
+
 export default class App extends React.Component {
+
+  state = initialState
+  
+
+
   render() {
+    console.log(todos)
+    
     return (
       <div>
         Todo App
         <ul>
-          <li>walk dog</li>
-          <li>gym</li>
-          <li>homework</li>
+          {
+          todos.map(todo => {
+            return (
+            <li key = {todo.id} > { todo.name } { todo.completed ? <span> - completed </span> : <span>  </span>} </li>
+            )
+          })
+          }
+          
         </ul>
 
         <form>
