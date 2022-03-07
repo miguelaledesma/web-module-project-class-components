47457cc3ce92f12d2d2856218c1a7506c5768c6b
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-07 14:44:09 -0800

    added list form and buttons, no functionality yet

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,17 @@ export default class App extends React.Component {
     return (
       <div>
         Todo App
+        <ul>
+          <li>walk dog</li>
+          <li>gym</li>
+          <li>homework</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>add</button>
+          <button>clear</button>
+        </form>
       </div>
     )
   }
